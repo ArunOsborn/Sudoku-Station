b02a310ab1bfddce1fa4b2690db2eca1a01ad4cf
Arun Osborn <55600843+ArunOsborn@users.noreply.github.com>
2024-03-29 15:44:24 +0000

    Square is now checked

--- solver.js
+++ solver.js
@@ -19,26 +19,36 @@ function solveSudoku()
     updateTableData();
     console.log(`Solving soduku with table: ${tableArray}`);
 
-    for (let i = 0; i < tableArray.length; i++) {
-        for (let j = 0; j < tableArray[i].length; j++)
+    for (let y = 0; y < tableArray.length; y++) {
+        for (let x = 0; x < tableArray[y].length; x++)
         {
-            if (tableArray[i][j] == "")
+            if (tableArray[y][x] == "")
             {
                 // Check row
-                const row = tableArray[i];
+                const row = tableArray[y];
                 possibilities = ["1","2","3","4","5","6","7","8","9"].filter(a => !row.includes(a));
+                
                 // Check column
-                const column = tableArray.map(row => row[j]);
+                const column = tableArray.map(row => row[x]);
                 possibilities = possibilities.filter(a => !column.includes(a));
-                tableElement.rows[i].cells[j].querySelector("input").ariaLabel = possibilities.join(", ");
-                console.log(`Cell ${i}, ${j} can be ${possibilities.join(", ")}`);
-                if (possibilities.length == 1)
-                {
-                    tableElement.rows[i].cells[j].querySelector("input").value = possibilities[0];
-                    tableElement.rows[i].cells[j].querySelector("input").style.backgroundColor = "lightgreen";
-                    tableElement.rows[i].cells[j].querySelector("input").style.color = "darkgrey";
-                }
+                tableElement.rows[y].cells[x].querySelector("input").ariaLabel = possibilities.join(", ");
+                console.log(`Cell (${x},${y}) can be ${possibilities.join(", ")}`);
+
+                // Check square
+                const squareRowStartIndex = Math.floor(x/3)*3;
+                const squareColumnStartIndex = Math.floor(y/3)*3;
+                let square = tableArray[squareColumnStartIndex].slice(squareRowStartIndex, squareRowStartIndex+3).concat(tableArray[squareColumnStartIndex+1].slice(squareRowStartIndex, squareRowStartIndex+3)).concat(tableArray[squareColumnStartIndex+2].slice(squareRowStartIndex, squareRowStartIndex+3));
+                console.log(`Square: ${square}`);
+                possibilities = possibilities.filter(a => !square.includes(a));
+                if (possibilities.length == 1) addSolvedSquare(y, x, possibilities[0]);
             }
         }
     }
+}
+
+function addSolvedSquare(i, j, value)
+{
+    tableElement.rows[i].cells[j].querySelector("input").value = possibilities[0];
+    tableElement.rows[i].cells[j].querySelector("input").style.backgroundColor = "lightgreen";
+    tableElement.rows[i].cells[j].querySelector("input").style.color = "darkgrey";
 }
